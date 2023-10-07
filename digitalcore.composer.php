da13f941eb5cfec4025f76ab9c74adf3f7f18be6
Near EarthObject <55352817+ephenyxshop@users.noreply.github.com>
2023-10-07 11:44:24 +0200

    1.1.3

---
+++ digitalcore.composer.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('DIGITAL_CORE_DIR')) {
+    define('DIGITAL_CORE_DIR', dirname(__FILE__));
+}
+if (!defined('DIGITAL_CORE_CLASS_DIR')) {
+    define('DIGITAL_CORE_CLASS_DIR', dirname(__FILE__).'/src/classes/');
+}
---
+++ src/classes/composer/shortcodes/ComposerShortCode_vc_blockcontact.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class ComposerShortCode_vc_blockcontact extends ComposerShortCode {
+
+	protected function outputTitle($title) {
+
+		return '';
+	}
+
+}
